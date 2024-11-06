63b63db9b0ccc230f3a24c3b94b3901869caead8
ROHAANK1708 <101565609+ROHAANK1708@users.noreply.github.com>
2024-11-06 14:24:50 +0530

    wd

--- json2html.js
+++ json2html.js
@@ -1,14 +1,20 @@
+
 export default function json2html(data) {
     if (!Array.isArray(data) || data.length === 0) {
         return "<table data-user='rohaan.khan11@gmail.com'><thead></thead><tbody></tbody></table>";
     }
+
     const headers = Array.from(new Set(data.flatMap(obj => Object.keys(obj))));
+
     let html = `<table data-user="rohaan.khan11@gmail.com">`;
+
     html += "<thead><tr>";
     headers.forEach(header => {
         html += `<th>${header}</th>`;
     });
     html += "</tr></thead><tbody>";
+
+
     data.forEach(row => {
         html += "<tr>";
         headers.forEach(header => {
@@ -16,6 +22,7 @@ export default function json2html(data) {
         });
         html += "</tr>";
     });
+
     html += "</tbody></table>";
     return html;
 }
